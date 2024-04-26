9b3fe3d5d3693eb51231281705f24fd93dfd1af8
luisaviveros <luisaviveros73@gmail.com>
2024-04-26 11:04:06 -0500

    lab11

---
+++ script.js
@@ -0,0 +1,27 @@
+const agregarPersonaje = (nombre, imagen) => {
+    const contenedor = document.getElementById("personajes");
+  
+    const personaje = document.createElement("div");
+    personaje.className = "personaje";
+    personaje.innerHTML = `
+      <h5 class="personaje__nombre">${nombre}</h5>
+      <img class="personaje__imagen" src="${imagen}" alt="${nombre}" />
+    `;
+  
+    contenedor.appendChild(personaje);
+  };
+  
+  const render = async () => {
+    try {
+      const response = await fetch('personajes.json');
+      const data = await response.json();
+      data.personajes.forEach(personaje => {
+        agregarPersonaje(personaje.name, personaje.image);
+      });
+    } catch (error) {
+      console.error('Error al cargar los personajes:', error);
+    }
+  };
+  
+  document.addEventListener("DOMContentLoaded", render);
+  
